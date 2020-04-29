e834f8cacd21fcb3cc4007e5eea982caf8919e2b
Mahesh Bandarupalli <mahesh10205@gmail.com>
2020-04-29 14:26:05 -0400

    my first commit to github

---
+++ AccountService/src/main/java/com/advance/act/account/controller/AccountController.java
@@ -0,0 +1,24 @@
+package com.advance.act.account.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@GetMapping("/status/check")
+	public String  getStatus() {
+		
+		return "I'm in account service" +" " +environment.getProperty("local.server.port");
+		
+	}
+
+}
